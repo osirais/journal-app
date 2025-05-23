ee664f45221bc53109a0302364976a9660c2f97b
maxhu08 <contact@maxhu.dev>
2025-05-22 19:39:08 -0500

    make more minimal

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -33,27 +33,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="flex min-h-screen flex-col items-center">
-            <div className="flex w-full flex-1 flex-col items-center gap-20">
-              <Navbar />
-              <div className="flex max-w-5xl flex-col gap-20 p-5">{children}</div>
-
-              <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
+          <Navbar />
+          <div className="min-h-screen px-8 py-20">{children}</div>
         </ThemeProvider>
       </body>
     </html>
