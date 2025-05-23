a8cd8962bf042463642561d0404554cf1fe787d0
maxhu08 <contact@maxhu.dev>
2025-05-22 20:35:11 -0500

    fix typo

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "jounral-app",
+  title: "journal-app",
   description: "TODO: Add description"
 };
 
