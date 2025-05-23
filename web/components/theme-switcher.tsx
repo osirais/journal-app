e50afc0551c2134ba4934e3b6e161c3970b0669e
maxhu08 <contact@maxhu.dev>
2025-05-23 10:25:14 -0500

    fix

--- web/components/theme-switcher.tsx
+++ web/components/theme-switcher.tsx
@@ -29,30 +29,26 @@ const ThemeSwitcher = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="cursor-pointer">
         <Button variant="ghost" size={"sm"}>
           {theme === "light" ? (
-            <Sun key="light" size={ICON_SIZE} className={"text-muted-foreground cursor-pointer"} />
+            <Sun key="light" size={ICON_SIZE} className={"text-muted-foreground"} />
           ) : theme === "dark" ? (
-            <Moon key="dark" size={ICON_SIZE} className={"text-muted-foreground cursor-pointer"} />
+            <Moon key="dark" size={ICON_SIZE} className={"text-muted-foreground"} />
           ) : (
-            <Laptop
-              key="system"
-              size={ICON_SIZE}
-              className={"text-muted-foreground cursor-pointer"}
-            />
+            <Laptop key="system" size={ICON_SIZE} className={"text-muted-foreground"} />
           )}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-content" align="start">
         <DropdownMenuRadioGroup value={theme} onValueChange={(e) => setTheme(e)}>
-          <DropdownMenuRadioItem className="flex gap-2" value="light">
+          <DropdownMenuRadioItem className="flex cursor-pointer gap-2" value="light">
             <Sun size={ICON_SIZE} className="text-muted-foreground" /> <span>Light</span>
           </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem className="flex gap-2" value="dark">
+          <DropdownMenuRadioItem className="flex cursor-pointer gap-2" value="dark">
             <Moon size={ICON_SIZE} className="text-muted-foreground" /> <span>Dark</span>
           </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem className="flex gap-2" value="system">
+          <DropdownMenuRadioItem className="flex cursor-pointer gap-2" value="system">
             <Laptop size={ICON_SIZE} className="text-muted-foreground" /> <span>System</span>
           </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
