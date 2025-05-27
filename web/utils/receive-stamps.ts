de198047b5b36c81a6036860fb175e9f7bfa4e30
maxhu08 <contact@maxhu.dev>
2025-05-27 13:02:43 -0500

    fix type error

--- web/utils/receive-stamps.ts
+++ web/utils/receive-stamps.ts
@@ -3,7 +3,9 @@ import { toast } from "sonner";
 export const receiveStamps = async (message: string) => {
   toast.success(message);
 
+  // @ts-ignore
   if (typeof window !== "undefined" && window.__refreshStamps) {
+    // @ts-ignore
     window.__refreshStamps();
   }
 };
