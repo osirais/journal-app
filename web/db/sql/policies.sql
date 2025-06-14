892bb3367801bc21f9c5a99db525f1050e56cdc1
Orlando <109116795+osirais@users.noreply.github.com>
2025-06-14 17:02:01 -0500

    organize

--- web/db/sql/policies.sql
+++ web/db/sql/policies.sql
@@ -1,13 +1,4 @@
 ALTER TABLE users ENABLE ROW LEVEL SECURITY;
-ALTER TABLE journal ENABLE ROW LEVEL SECURITY;
-ALTER TABLE entry ENABLE ROW LEVEL SECURITY;
-ALTER TABLE tag ENABLE ROW LEVEL SECURITY;
-ALTER TABLE entry_tag ENABLE ROW LEVEL SECURITY;
-ALTER TABLE user_balance ENABLE ROW LEVEL SECURITY;
-ALTER TABLE balance_transaction ENABLE ROW LEVEL SECURITY;
-ALTER TABLE user_settings ENABLE ROW LEVEL SECURITY;
-ALTER TABLE reason ENABLE ROW LEVEL SECURITY;
-
 DROP POLICY IF EXISTS "Users can access their own user row" ON users;
 CREATE POLICY "Users can access their own user row"
   ON users FOR ALL
@@ -15,6 +6,7 @@ CREATE POLICY "Users can access their own user row"
     id = (SELECT auth.uid())
   );
 
+ALTER TABLE journal ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own journals" ON journal;
 CREATE POLICY "Users can access their own journals"
   ON journal FOR ALL
@@ -22,6 +14,7 @@ CREATE POLICY "Users can access their own journals"
     author_id = (SELECT auth.uid())
   );
 
+ALTER TABLE entry ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own entries" ON entry;
 CREATE POLICY "Users can access their own entries"
   ON entry FOR ALL
@@ -31,6 +24,7 @@ CREATE POLICY "Users can access their own entries"
     )
   );
 
+ALTER TABLE tag ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own tags" ON tag;
 CREATE POLICY "Users can access their own tags"
   ON tag FOR ALL
@@ -38,6 +32,7 @@ CREATE POLICY "Users can access their own tags"
     user_id = (SELECT auth.uid())
   );
 
+ALTER TABLE entry_tag ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access entry_tag of their own entries" ON entry_tag;
 CREATE POLICY "Users can access entry_tag of their own entries"
   ON entry_tag FOR ALL
@@ -50,18 +45,21 @@ CREATE POLICY "Users can access entry_tag of their own entries"
     )
   );
 
-DROP POLICY IF EXISTS "Users can access their own user balances" ON user_balance;
+ALTER TABLE user_balance ENABLE ROW LEVEL SECURITY;
+DROP POLICY IF EXISTS "Users can access their own balances" ON user_balance;
 CREATE POLICY "Users can access their own balances"
   ON user_balance FOR ALL
   USING (
     user_id = (SELECT auth.uid())
   );
 
+ALTER TABLE balance_transaction ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own balance transactions" ON balance_transaction;
 CREATE POLICY "Users can access their own balance transactions"
   ON balance_transaction FOR ALL
   USING (user_id = (SELECT auth.uid()));
 
+ALTER TABLE user_settings ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own settings" ON user_settings;
 CREATE POLICY "Users can access their own settings"
   ON user_settings FOR ALL
@@ -69,6 +67,7 @@ CREATE POLICY "Users can access their own settings"
     user_id = (SELECT auth.uid())
   );
 
+ALTER TABLE reason ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own reasons" ON reason;
 CREATE POLICY "Users can access their own reasons"
   ON reason FOR ALL
@@ -76,6 +75,7 @@ CREATE POLICY "Users can access their own reasons"
     user_id = (SELECT auth.uid())
   );
 
+ALTER TABLE mood_entry ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own mood entries" ON mood_entry;
 CREATE POLICY "Users can access their own mood entries"
   ON mood_entry FOR ALL
@@ -83,6 +83,7 @@ CREATE POLICY "Users can access their own mood entries"
     user_id = (SELECT auth.uid())
   );
 
+ALTER TABLE streak ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS "Users can access their own streaks" ON streak;
 CREATE POLICY "Users can access their own streaks"
   ON streak FOR ALL
