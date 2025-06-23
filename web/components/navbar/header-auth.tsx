9b1212a61911ada4fb4f113a307ef1461ebbcfde
Orlando <109116795+osirais@users.noreply.github.com>
2025-06-22 21:23:55 -0500

    tweak login button

--- web/components/navbar/header-auth.tsx
+++ web/components/navbar/header-auth.tsx
@@ -25,15 +25,10 @@ export default async function AuthButton() {
 
   if (!user) {
     return (
-      <Button
-        variant="ghost"
-        className="hover:text-foreground flex cursor-pointer items-center gap-2 hover:bg-white/20"
-      >
+      <Button variant="ghost" asChild>
         <Link href="/login">
-          <div className="flex items-center gap-1">
-            <LogIn size={16} />
-            <span>Sign In</span>
-          </div>
+          <LogIn />
+          <span>Sign in</span>
         </Link>
       </Button>
     );
